a51062fb01b8eba3b57ed59b271de6418a2051ff
adorsk <adorsk>
2013-01-13 13:51:05 -0500

    fixed entrypoint

--- jython_runner/templates/entrypoint.py
+++ jython_runner/templates/entrypoint.py
@@ -1,4 +1,12 @@
+# Add setuptools egg to path.
+import sys 
+import os
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(this_dir, 'setuptools.egg'))
 from sasi_gridder import jython_gui
 
 def main():
     jython_gui.main()
+
+if __name__ == '__main__':
+    main()
